954a04deb6d1b2695d24408fe3fdef9b46a6fe9e
SujjithS <sujjiths@prod.hclpnp.com>
2021-03-27 11:13:01 +0530

    Python Excel Operation

--- TestPgm.py
+++ TestPgm.py
@@ -1,2 +1,14 @@
-print("Hello"+" Sujjith")
-print("How are you")
+from openpyxl import load_workbook
+
+wb = load_workbook(filename='CatalogEntries-Vita.xlsx') #load workbook from the location
+wb.active=2
+ws = wb.active
+# List sheets available
+sheets = wb.get_sheet_names()
+print(sheets)
+
+# Load active sheet or named sheet
+sheet = wb.active
+# sheet = wb['User Information']
+# Read a specific cell
+print(sheet['J4'].value)
