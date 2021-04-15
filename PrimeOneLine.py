9d2a899ac49e55c8ccab2a8387a37db1cb667983
SujjithS <SujjithS@hcl.com>
2021-04-15 16:51:57 +0530

    One line expression for Prime

---
+++ PrimeOneLine.py
@@ -0,0 +1,9 @@
+c=list(map(lambda x:all(x % y!=0 for y in range(2,x)),range(2,13)))
+#list(map(int, c)) #converts to 0's and 1's
+Primecount=0
+for i in c:
+  if(i==True):
+    Primecount+=1
+  else:
+    continue
+print("Number of prime numbers",Primecount)
